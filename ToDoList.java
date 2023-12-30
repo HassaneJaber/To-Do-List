7fdc6b97c877d3ba6319d3ee9de54c0f1c7e56f9
HassaneJaber <146438988+HassaneJaber@users.noreply.github.com>
2023-12-30 09:01:36 +0200

    Create ToDoList.java

---
+++ ToDoList.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ToDoList {
+    private ArrayList<String> tasks;
+
+    public ToDoList() {
+        tasks = new ArrayList<>();
+    }
+
+    public void addTask(String task) {
+        tasks.add(task);
+        System.out.println("Task added: " + task);
+    }
+
+    public void removeTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            String removedTask = tasks.remove(index);
+            System.out.println("Task removed: " + removedTask);
+        } else {
+            System.out.println("Invalid index. No task removed.");
+        }
+    }
+
+    public void displayTasks() {
+        if (tasks.isEmpty()) {
+            System.out.println("No tasks in the to-do list.");
+        } else {
+            System.out.println("To-Do List:");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println((i + 1) + ". " + tasks.get(i));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ToDoList toDoList = new ToDoList();
+
+        while (true) {
+            System.out.println("\nChoose an option:");
+            System.out.println("1. Add Task");
+            System.out.println("2. Remove Task");
+            System.out.println("3. Display Tasks");
+            System.out.println("4. Exit");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter task: ");
+                    scanner.nextLine(); // Consume the newline character
+                    String task = scanner.nextLine();
+                    toDoList.addTask(task);
+                    break;
+                case 2:
+                    System.out.print("Enter index to remove: ");
+                    int index = scanner.nextInt();
+                    toDoList.removeTask(index - 1);
+                    break;
+                case 3:
+                    toDoList.displayTasks();
+                    break;
+                case 4:
+                    System.out.println("Exiting program. Goodbye!");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+}
